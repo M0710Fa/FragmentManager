486ec880a6a2918711665f8d8eaa8797726f2400
M0710Fa <yuya.miyata@zozo.com>
2024-02-04 19:09:21 +0900

    こっみｔ

--- app/src/main/java/com/example/fragmentmanager/model/DogRemoteDataSource.kt
+++ app/src/main/java/com/example/fragmentmanager/model/DogRemoteDataSource.kt
@@ -1,11 +1,31 @@
 package com.example.fragmentmanager.model
 
+import retrofit2.HttpException
 import retrofit2.Response
 
 class DogRemoteDataSource {
-    suspend fun getRandomDog(): Result<Dog> {
+    suspend fun getRandomDog() {
+        runCatching {
+            apiParse(dogsApiService.getRandomDog())
+        }.onSuccess {
+            Result.success(it)
+        }.onFailure {
+            throw it
+        }
     }
 }
+//
+//sealed interface DogResult(T) {
+//    data class Success(val data: T): DogResult<T>
+//}
 
-fun <T> apiParse(data : Response<T>): T{
+fun <T> apiParse(response : Response<T>): T?{
+    return when(response.isSuccessful) {
+        true -> {
+            response.body()
+        }
+        false -> {
+            throw HttpException(response)
+        }
+    }
 }
